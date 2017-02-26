7b5926d5656abced3a83a7080fd26d7ec84cfeaa
Vignesh Vasu <vigneshsvasu@berkeley.edu>
2017-02-26 15:47:46 -0800

    toString added to table

--- db/Table.java
+++ db/Table.java
@@ -89,4 +89,17 @@ public class Table {
         }
         return newArray;
     }
+
+    public String toString(){
+      String returnString = " ";
+      for (int i = 0; i < columns[0].values.length; i++){
+        for (int j = 0; j < columns.length; j++){
+          if (j = columns.length - 1){
+            returnString += columns[i].values.get(j);
+          }
+          returnString = returnString + columns[i].values.get(j) + ",";
+        }
+        returnString = returnString + "\n";
+      }
+    }
 }
