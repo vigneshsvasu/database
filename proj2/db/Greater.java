7694b9a4148d95e5a9a65bcf8149b3c23ef371a0
Vignesh Vasu <vigneshsvasu@berkeley.edu>
2017-03-05 14:52:30 -0800

    UnaryCE

---
+++ proj2/db/Greater.java
@@ -0,0 +1,39 @@
+package db;
+
+/**
+ * Created by vigneshvasu on 3/5/17.
+ */
+public class Greater {
+    public boolean apply(Value left, Value right){
+        return false;
+    }
+    public boolean apply(FloatValue left, FloatValue right){
+        double leftVal = left.getValue();
+        double rightVal = right.getValue();
+        return leftVal > rightVal;
+    }
+    public boolean apply(IntValue left, IntValue right){
+        int leftVal = left.getValue();
+        int rightVal = right.getValue();
+        return leftVal > rightVal;
+    }
+    public boolean apply(FloatValue left, IntValue right){
+        double newVal = left.getValue();
+        double other = (double) right.getValue();
+        return newVal > other;
+    }
+    public boolean apply(IntValue left, FloatValue right){
+        double newVal = right.getValue();
+        double other = (double) left.getValue();
+        return newVal < other;
+    }
+    public boolean apply(StringValue left, StringValue right){
+        String lef = left.getValue();
+        String rig = right.getValue();
+        int val = lef.compareTo(rig);
+        if (val > 0){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ proj2/db/NotEqual.java
@@ -0,0 +1,35 @@
+package db;
+
+/**
+ * Created by vigneshvasu on 3/5/17.
+ */
+public class NotEqual {
+    public boolean apply(Value left, Value right){
+        return false;
+    }
+    public boolean apply(FloatValue left, FloatValue right){
+        double leftVal = left.getValue();
+        double rightVal = right.getValue();
+        return leftVal != rightVal;
+    }
+    public boolean apply(IntValue left, IntValue right){
+        int leftVal = left.getValue();
+        int rightVal = right.getValue();
+        return leftVal != rightVal;
+    }
+    public boolean apply(FloatValue left, IntValue right){
+        double newVal = left.getValue();
+        double other = (double) right.getValue();
+        return newVal != other;
+    }
+    public boolean apply(IntValue left, FloatValue right){
+        double newVal = right.getValue();
+        double other = (double) left.getValue();
+        return newVal != other;
+    }
+    public boolean apply(StringValue left, StringValue right){
+        String lef = left.getValue();
+        String rig = right.getValue();
+        return !(lef.equals(rig));
+    }
+}
