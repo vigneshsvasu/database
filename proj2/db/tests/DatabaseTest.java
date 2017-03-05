9cce6ab6412f2eadbc561c1e9212a27beba1c18f
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-03-05 14:43:25 -0800

    Added insertion tests, select test stubs

--- proj2/db/tests/DatabaseTest.java
+++ proj2/db/tests/DatabaseTest.java
@@ -39,7 +39,7 @@ public class DatabaseTest {
         message = db.transact("create table example (z float)");
         assertTrue(message.startsWith("ERROR"));
 
-        // TODO: add selects
+        // TODO: create table with select
     }
 
     @Test
@@ -51,5 +51,45 @@ public class DatabaseTest {
 
         message = db.transact("create table example (x int)");
         assertTrue(db.transact("drop table example").isEmpty());
+        assertTrue(db.transact("drop table example").startsWith("ERROR"));
+    }
+
+    @Test
+    public void testInsert() {
+        Database db = new Database();
+
+        String message = db.transact("create table example (x int,y string)");
+        assertTrue(message.isEmpty());
+        assertEquals("x int,y string", db.transact("print example"));
+
+        message = db.transact("insert into example values (NAN, 'Hello')");
+        assertTrue(message.isEmpty());
+
+        message = db.transact("insert into example (5, 'World')");
+        assertTrue(message.startsWith("ERROR"));
+
+        message = db.transact("print example");
+        assertEquals("x int,y string\nNAN,'Hello'", message);
+
+        message = db.transact("insert into example values (-5, NOVALUE)");
+        assertTrue(message.isEmpty());
+
+        message = db.transact("print example");
+        assertEquals("x int,y string\nNAN,'Hello'\n-5,NOVALUE", message);
+    }
+
+    @Test
+    public void testSelect() {
+        // TODO: when select is done
+    }
+
+    @Test
+    public void testSelectWithJoin() {
+        // TODO: when select is done
+    }
+
+    @Test
+    public void testConditionalSelect() {
+        // TODO: when select is done
     }
 }
